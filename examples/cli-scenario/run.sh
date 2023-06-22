65f9e38e158858e8417a2121f8e21cb7249bfc84
Lucas <31957045+SketchingDev@users.noreply.github.com>
2023-06-22 15:20:00 -0600

    Update CLI example

---
+++ examples/cli-scenario/run.sh
@@ -0,0 +1,13 @@
+#sh
+
+# exit when any command fails
+set -e
+
+npm install -g ../../packages/genesys-web-messaging-tester-cli
+
+function cleanup {
+  npm uninstall -g ../../packages/genesys-web-messaging-tester-cli
+}
+trap cleanup EXIT
+
+web-messaging-tester test-scenario example.yml -id $DEPLOYMENT_ID -r $REGION -p 10
