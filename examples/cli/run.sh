840d9204209b3f8467d18fc7c1d65b9a7967e1d2
Lucas <31957045+SketchingDev@users.noreply.github.com>
2023-07-18 21:45:03 +0100

    Revert change

--- examples/cli/run.sh
+++ examples/cli/run.sh
@@ -10,7 +10,4 @@ function cleanup {
 }
 trap cleanup EXIT
 
-# Useful to run here so I can copy/paste when updating CLI help in docs
-web-messaging-tester --help
-
 web-messaging-tester example.yml -id $DEPLOYMENT_ID -r $REGION -p 10
