b96182f813888ebe09259ae9e7ceb1c4ddb3ef2d
Lucas <31957045+SketchingDev@users.noreply.github.com>
2023-07-20 22:40:48 +0100

    Add example to test exit codes (#57)

--- examples/cli/run.sh
+++ examples/cli/run.sh
@@ -1,8 +1,5 @@
 #sh
 
-# exit when any command fails
-set -e
-
 npm install -g ../../packages/genesys-web-messaging-tester-cli
 
 function cleanup {
@@ -10,4 +7,24 @@ function cleanup {
 }
 trap cleanup EXIT
 
-web-messaging-tester example.yml -id $DEPLOYMENT_ID -r $REGION -p 10
+## Passing example exits with exit code 0
+web-messaging-tester example-pass.yml -id $DEPLOYMENT_ID -r $REGION -p 10
+if [ $? -ne 0 ]
+then
+  echo ""
+  echo "========="
+  echo "UNEXPECTED EXIT CODE $? - Passing example did not result in Exit Code of 0"
+  exit 1
+fi
+
+## Failing example exits with exit code 1
+web-messaging-tester example-fail.yml -id $DEPLOYMENT_ID -r $REGION -p 10
+if [ $? -ne 1 ]
+then
+  echo ""
+  echo "========="
+  echo "UNEXPECTED EXIT CODE $? - Intentionally failing example did not result in Exit Code of 1"
+  exit 1
+fi
+
+exit 0
