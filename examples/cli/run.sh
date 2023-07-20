197273480f9f6264a45afa7bff7012074551fd62
Lucas <31957045+SketchingDev@users.noreply.github.com>
2023-07-20 22:11:42 +0100

    Add test for exit codes

--- examples/cli/run.sh
+++ examples/cli/run.sh
@@ -1,7 +1,8 @@
 #sh
-
-# exit when any command fails
 set -e
+
+RED='\033[0;31m'
+NO_COLOUR='\033[0m'
 
 npm install -g ../../packages/genesys-web-messaging-tester-cli
 
@@ -10,4 +11,18 @@ function cleanup {
 }
 trap cleanup EXIT
 
-web-messaging-tester example.yml -id $DEPLOYMENT_ID -r $REGION -p 10
+## Passing test exits with code 0
+web-messaging-tester example-pass.yml -id $DEPLOYMENT_ID -r $REGION -p 10
+if [ $? -ne 0 ]
+then
+  echo "${RED}Passing test did not result in Exit Code of 0. Exit code was $?${NO_COLOUR}"
+  exit 1
+fi
+
+## Failing test exists with code 1
+web-messaging-tester example-fail.yml -id $DEPLOYMENT_ID -r $REGION -p 10
+if [ $? -ne 1 ]
+then
+  echo "${RED}Failing test did not result in Exit Code of 1. Exit code was $?${NO_COLOUR}"
+  exit 1
+fi
