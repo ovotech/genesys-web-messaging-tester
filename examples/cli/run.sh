1a7b603e967641248f29a412bd788015970d2461
Lucas <31957045+SketchingDev@users.noreply.github.com>
2023-07-20 22:33:36 +0100

    Improve wording, but still fail

--- examples/cli/run.sh
+++ examples/cli/run.sh
@@ -1,8 +1,5 @@
 #sh
 
-RED='\033[0;31m'
-NO_COLOUR='\033[0m'
-
 npm install -g ../../packages/genesys-web-messaging-tester-cli
 
 function cleanup {
@@ -14,7 +11,8 @@ trap cleanup EXIT
 web-messaging-tester example-pass.yml -id $DEPLOYMENT_ID -r $REGION -p 10
 if [ $? -ne 0 ]
 then
-  echo "\n\n${RED}Passing test did not result in Exit Code of 0. Exit code was $?${NO_COLOUR}"
+  echo "========="
+  echo "UNEXPECTED EXIT CODE $? - Failing test did not result in Exit Code of 0"
   exit 1
 fi
 
@@ -22,7 +20,8 @@ fi
 web-messaging-tester example-fail.yml -id $DEPLOYMENT_ID -r $REGION -p 10
 if [ $? -ne 0 ]
 then
-  echo "\n\n${RED}Failing test did not result in Exit Code of 1. Exit code was $?${NO_COLOUR}"
+  echo "========="
+  echo "UNEXPECTED EXIT CODE $? - Failing test did not result in Exit Code of 1"
   exit 1
 fi
 
